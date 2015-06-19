f7a92dd34c8f1618d58ba7b0ce221b85c7cfd783
Daniel Berger <dberger@redhat.com>
2015-06-19 06:45:07 -0600

    Added some singleton tests.

--- spec/rslip_spec.rb
+++ spec/rslip_spec.rb
@@ -9,7 +9,7 @@ require 'rslp'
 describe "rslp" do
   before{
     @lang = 'en-us'
-    @slp = OpenSLP::SLP.new(@lang)
+    @slp = OpenSLP::SLP.new(@lang, false)
   }
 
   context "constructor" do
@@ -19,6 +19,46 @@ describe "rslp" do
 
     it "defaults to a false async value if no value is provided" do
       expect(OpenSLP::SLP.new.async).to be_false
+    end
+
+    it "sets attributes to expected values" do
+      expect(@slp.lang).to eq(@lang)
+      expect(@slp.async).to be_false
+    end
+  end
+
+  context "singleton methods" do
+    it "defines a refresh_interval method" do
+      expect(OpenSLP::SLP).to respond_to(:refresh_interval)
+    end
+
+    it "returns the expected value for refresh_interval" do
+      expect(OpenSLP::SLP.refresh_interval).to eq(0)
+    end
+
+    it "defines a get_property method" do
+      expect(OpenSLP::SLP).to respond_to(:get_property)
+    end
+
+    it "defines a set_property method" do
+      expect(OpenSLP::SLP).to respond_to(:set_property)
+    end
+
+    it "defines a parse_service_url method" do
+      expect(OpenSLP::SLP).to respond_to(:parse_service_url)
+    end
+
+    it "defines a escape_reserved method" do
+      expect(OpenSLP::SLP).to respond_to(:escape_reserved)
+    end
+
+    it "returns the expected value for the escape_reserved method" do
+      expected = "\\2Ctag-example\\2C"
+      expect(OpenSLP::SLP.escape_reserved(",tag-example,")).to eq(expected)
+    end
+
+    it "defines a unescape_reserved method" do
+      expect(OpenSLP::SLP).to respond_to(:unescape_reserved)
     end
   end
 
